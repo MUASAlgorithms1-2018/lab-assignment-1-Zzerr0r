e76fa5ae859bcfbb5bacbd5a84b5e2b85e509ad0
Zzerr0r <43878904+Zzerr0r@users.noreply.github.com>
2018-10-12 20:37:23 +0200

    Subsequence n^1 algorithm

---
+++ cmake-build-debug/CMakeFiles/SortingPro.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SortingPro.dir/main.cpp.obj"
+  "SortingPro.pdb"
+  "SortingPro.exe"
+  "SortingPro.exe.manifest"
+  "libSortingPro.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SortingPro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
